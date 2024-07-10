8a0877e4c1fdfe9639ec5584a51b7b12183469be
BriBriMurray <loveandintention1@gmail.com>
2024-07-10 19:17:53 -0400

    Fixed bugs adding commas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 2.4, type: ['grass, poison']}
-    { name: 'Girafarig', height: 5, type: ['psychic']}
-    { name: 'Hydrogenhydreigon', height: 6, type: ['dark']}
+    { name: 'Bulbasaur', height: 2.4, type: ['grass, poison']},
+    { name: 'Girafarig', height: 5, type: ['psychic']},
+    { name: 'Hydrogenhydreigon', height: 6, type: ['dark']},
 ]
