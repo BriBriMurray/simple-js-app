ff37249f8f41a567cda2a605c9bfcac5e5767244
BriBriMurray <loveandintention1@gmail.com>
2024-07-10 19:11:23 -0400

    Added first array of my pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Avocado';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 2.4, type: ['grass, poison']}
+    { name: 'Girafarig', height: 5, type: ['psychic']}
+    { name: 'Hydrogenhydreigon', height: 6, type: ['dark']}
+]
