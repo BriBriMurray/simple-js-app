20285e270fe2c2fc72db81d919a523f725eb2edc
BriBriMurray <loveandintention1@gmail.com>
2024-07-11 19:37:34 -0400

    fixed typo

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 2.4, type: ['grass, poison']},
-    { name: 'Girafarig', height: 5, type: ['psychic']},
-    { name: 'Hydrogenhydreigon', height: 6, type: ['dark']},
+    { name: 'Bulbasaur', height: 2.4, types: ['grass, poison']},
+    { name: 'Girafarig', height: 5, types: ['psychic']},
+    { name: 'Hydrogenhydreigon', height: 6, types: ['dark']},
 ]
