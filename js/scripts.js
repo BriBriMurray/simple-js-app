45199afd1ac2011c9e2701a1928afaa6b9a9cbf2
BriBriMurray <loveandintention1@gmail.com>
2024-08-08 13:04:43 -0400

    Added forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 2.4, types: ['grass, poison']},
-    { name: 'Girafarig', height: 5, types: ['psychic']},
-    { name: 'Hydrogenhydreigon', height: 6, types: ['dark']},
-]
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 2.4, types: ['grass, poison']},
+        { name: 'Girafarig', height: 5, types: ['psychic']},
+        { name: 'Hydrogenhydreigon', height: 6, types: ['dark']},
+    ];
+    function getAll() {
+        return pokemonList;
+    }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    return{
+        getAll: getAll,
+        add: add
+    };
+    })();
+    
+    pokemonRepository.getAll().forEach(funtion(pokemon) {
+        document.write( pokemon.name + ' (height: ' + pokemon.height + ')');
+        if (pokemon.height > 5) {
+            document.write (' Wow! That\'s big!');
+        }
+    });
+
+
+
