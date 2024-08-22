4e12417f8726aa559f9ca6deb3a83c8c4782aaf4
BriBriMurray <loveandintention1@gmail.com>
2024-08-21 20:50:29 -0400

    added promise and fetch pollyfill

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 let pokemonRepository = (function () {
 
     let repository = [
-        { name: 'Bulbasaur', height: 2.4, types: ['grass, poison']},
-        { name: 'Girafarig', height: 5, types: ['psychic']},
-        { name: 'Hydrogenhydreigon', height: 6, types: ['dark']},
+        
     ];
     function add(pokemon) {
         if (
